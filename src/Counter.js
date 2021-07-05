6731733dfab9ceb425a5ae9270f26fe16dfa0c95
hyunsic <>
2021-07-05 20:40:37 +0900

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,35 @@
+import React,{Component} from 'react';
+
+class Counter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            number: 0,
+            fixedNumber: 0
+        };
+    }
+    render(){
+        const {number, fixedNumber} = this.state;
+        return (
+            <div>
+                <h1>{number}</h1>
+                <h2>{fixedNumber}</h2>
+                <button
+                onClick={() => {
+                    this.setState(
+                        {
+                            number:number+1
+                        }, () => {
+                            console.log(this.state);
+                        }
+                    );
+                }}
+                >
+                    +1
+                </button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
